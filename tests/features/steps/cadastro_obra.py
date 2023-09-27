227b33dab82f2183ee89ce5d621d800e368e5abe
Ychristino <ychristino@gmail.com>
2023-09-27 20:33:17 -0300

    so quero que funcione

--- tests/features/steps/cadastro_obra.py
+++ tests/features/steps/cadastro_obra.py
@@ -2,15 +2,17 @@ from behave import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-import time
+from pathlib import Path
 
 driver = webdriver.Chrome()
 
 @given('Acessou o menu de cadastro de obra')
 def step_impl(context):
     print('Acessando a tela de cadastro de obra')
-    time.sleep(5)
-    driver.get("file:///C:/Users/Yan/Documents/projetos/html_css_js/Gerenciamento%20de%20Custo/index.html")
+    
+    html_page = Path.cwd() / 'index.html'
+    driver.get(html_page.as_uri())
+    
     menu = driver.find_element(By.ID, 'nova_obra')
     menu.click()
 
@@ -22,7 +24,6 @@ def step_impl(context):
     bairro = driver.find_element(By.ID, 'bairro')
     cidade = driver.find_element(By.ID, 'cidade')
     estado = Select(driver.find_element(By.ID, 'estado'))
-    time.sleep(5)
 
     logradouro.send_keys("Rua da minha nova obra")
     numero.send_keys("2000")
@@ -31,15 +32,12 @@ def step_impl(context):
     cidade.send_keys("Porto Alegre")
     estado.select_by_value("rs")
     print('Preenchi minhas inforamções')
-    time.sleep(5)
 
 
 @then('A tela de cadastro da obra e concluida')
 def step_impl(context):
-    time.sleep(5)
     concluir = driver.find_element(By.ID, 'nextBtn')
     concluir.click()
     print('Salvei o registro')
     driver.find_element(By.ID, 'nova_obra')
     print('Voltou para o menu')
-    time.sleep(60)
